ca61b4566c9f6c0b5ef794ff6e1519e214060286
Luke Bayliss <lpbayliss@users.noreply.github.com>
2024-01-03 17:09:27 +1100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
+import { FormattedMessage } from 'react-intl';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Starter App</title>
+        <meta name="description" content="A Project Starter App" />
+      </Head>
+      <Container as="main">
+        <Box>
+          <Heading>
+            <FormattedMessage defaultMessage="Starter App Project" description="Name of project" id="+rSl7d" />
+          </Heading>
+          <Text>
+            <FormattedMessage defaultMessage="Hello world" description="Hello message" id="RsbSsm" />
+          </Text>
+        </Box>
+      </Container>
+    </>
+  );
+}
